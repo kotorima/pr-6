c86f254c425a9da08584f2ded0d213c51e9f6397
kotorima <35659278+kotorima@users.noreply.github.com>
2019-03-21 11:26:41 +0300

    ready

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+Vue.component('movie-search', {
+  template: `
+  <header>
+    <input type="text" v-model="value">
+    <button v-on:click="valueSend">SEARCH</button>
+  </header>`,
+  data() {
+    return{
+      value: '',
+    }
+  },
+  methods: {
+    valueSend: function () {
+      fetch('http://www.omdbapi.com/?s='+this.value+'&apikey=d5677312')
+      .then((response) => {
+          return response.json();
+      })
+      .then((result) => {
+          if(result.Response == 'True'){
+            this.$root.films = result.Search;
+            this.$root.ok = true;
+          } else {
+            this.$root.ok = false;
+          }
+      })
+    }
+  }
+})
 
 Vue.component('movie', {
   template: `
@@ -14,7 +42,12 @@ Vue.component('movie', {
 Vue.component('movie-show', {
   template: `
   <main>
+    <div v-if='ok' class="films">
       <movie v-for="film in movielist" v-bind:movieitem="film"></movie>
+    </div>
+    <div v-else>
+      <p class="error">Ooooops, films not found</p>
+    </div>
   </main>`,
   props: ['movielist', 'ok']
 });
@@ -22,23 +55,7 @@ Vue.component('movie-show', {
 var app = new Vue({
   el: '#app',
   data: {
-      value: '',
-      films : [],
-      ok: true
-    },
-    methods: {
-      valueSend: function () {
-        fetch('http://www.omdbapi.com/?s='+this.value+'&apikey=d5677312')
-        .then((response) => {
-            return response.json();
-        })
-        .then((result) => {
-            if(result.Response == 'True'){
-                this.dataSearch = result.Search;
-            } else {
-                this.ok = false;
-            }
-        })
-      }
+    ok: true,
+    films: []
     }
   }) 
